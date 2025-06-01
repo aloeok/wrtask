51d5e26409994860f5f98f2a3d7ed08a203cf4fb
loenko_am <LoenkoAM@transport.mos.ru>
2025-06-01 21:04:48 +0300

    UserSubscriptionService with main app logic

---
+++ src/main/java/ru/aloeok/wrtask/service/UserSubscriptionService.java
@@ -0,0 +1,103 @@
+package ru.aloeok.wrtask.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.aloeok.wrtask.entity.SubscriptionEntity;
+import ru.aloeok.wrtask.entity.UserEntity;
+import ru.aloeok.wrtask.model.Subscription;
+import ru.aloeok.wrtask.model.User;
+import ru.aloeok.wrtask.repository.SubscriptionRepository;
+import ru.aloeok.wrtask.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UserSubscriptionService {
+    private final UserRepository userRepository;
+    private final SubscriptionRepository subscriptionRepository;
+
+    @Transactional
+    public @NotNull Long createUser(@NotNull String username) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername(username);
+        userRepository.save(userEntity);
+        return userEntity.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public @NotNull User getUser(@NotNull Long id) {
+        return userRepository.findById(id)
+                .map(userEntity -> new User(
+                        userEntity.getId(),
+                        userEntity.getUsername()
+                ))
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "User not found"
+                ));
+    }
+
+    @Transactional
+    public void updateUser(@NotNull Long id, @NotNull String username) {
+        UserEntity userEntity = userRepository.getReferenceById(id);
+        userEntity.setUsername(username);
+    }
+
+    @Transactional
+    public void deleteUser(@NotNull Long id) {
+        userRepository.deleteById(id);
+    }
+
+
+    @Transactional
+    public @NotNull Long createSubscription(@NotNull String name) {
+        SubscriptionEntity subscriptionEntity = new SubscriptionEntity();
+        subscriptionEntity.setName(name);
+        subscriptionRepository.save(subscriptionEntity);
+        return subscriptionEntity.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public void deleteSubscription(@NotNull Long id) {
+        subscriptionRepository.deleteById(id);
+    }
+
+
+    @Transactional
+    public void addUserSubscription(@NotNull Long userId, @NotNull Long subscriptionId) {
+        UserEntity userEntity = userRepository.getReferenceById(userId);
+        SubscriptionEntity subscriptionEntity = subscriptionRepository.getReferenceById(subscriptionId);
+        userEntity.getSubscriptions().add(subscriptionEntity);
+    }
+
+    @Transactional(readOnly = true)
+    public @NotNull List<Subscription> getUserSubscriptions(@NotNull Long userId) {
+        return subscriptionRepository.findByUsersId(userId).stream()
+                .map(subscriptionEntity -> new Subscription(
+                        subscriptionEntity.getId(),
+                        subscriptionEntity.getName()
+                ))
+                .toList();
+    }
+
+    @Transactional
+    public void deleteUserSubscription(@NotNull Long userId, @NotNull Long subscriptionId) {
+        UserEntity userEntity = userRepository.getReferenceById(userId);
+        SubscriptionEntity subscriptionEntity = subscriptionRepository.getReferenceById(subscriptionId);
+        userEntity.getSubscriptions().remove(subscriptionEntity);
+    }
+
+    @Transactional(readOnly = true)
+    public @NotNull List<Subscription> getTop3Subscriptions() {
+        return subscriptionRepository.findSortedByUserCount(Pageable.ofSize(3)).stream()
+                .map(subscriptionEntity -> new Subscription(
+                        subscriptionEntity.getId(),
+                        subscriptionEntity.getName()
+                ))
+                .toList();
+    }
+}
