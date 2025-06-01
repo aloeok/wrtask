978bf0165ee203e84f621ddad17add459d43abe5
loenko_am <LoenkoAM@transport.mos.ru>
2025-06-01 21:03:46 +0300

    task-specific query methods

--- src/main/java/ru/aloeok/wrtask/repository/SubscriptionRepository.java
+++ src/main/java/ru/aloeok/wrtask/repository/SubscriptionRepository.java
@@ -1,10 +1,22 @@
 package ru.aloeok.wrtask.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.aloeok.wrtask.entity.SubscriptionEntity;
 
+import java.util.List;
+
 @Repository
 public interface SubscriptionRepository extends JpaRepository<SubscriptionEntity, Long> {
+    List<SubscriptionEntity> findByUsersId(Long userId);
 
+    @Query("""
+    select s from subscriptions s
+    left join s.users u
+    group by s
+    order by count(u) desc
+    """)
+    List<SubscriptionEntity> findSortedByUserCount(Pageable pageable);
 }
